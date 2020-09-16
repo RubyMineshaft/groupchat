e95853640563f048ce3edcb19a001a61218a4cfe
mattetress <mattetress@me.com>
2020-09-16 17:01:55 -0500

    add name validation to channel

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,4 +3,5 @@ class Channel < ApplicationRecord
   has_many :users, through: :channel_users, dependent: :destroy
   has_many :messages, dependent: :destroy
 
+  validates :name, presence: true 
 end
